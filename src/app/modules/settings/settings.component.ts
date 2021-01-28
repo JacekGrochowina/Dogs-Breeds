775984f849d47e5ee347a3ff4fe9e87eb253bc5b
Jacek Grochowina <jacek99@vp.pl>
2021-01-28 09:47:16 +0100

    feat(app): add basic ui

--- src/app/modules/settings/settings.component.ts
+++ src/app/modules/settings/settings.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Range {
+  min: number;
+  max: number;
+}
+
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
@@ -7,9 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  range: Range = {
+    min: 1,
+    max: 100
+  }
+  numberPhoto: number = 5;
+  language: string = 'pl';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  checkValue(): void {
+    if(this.numberPhoto <= this.range.min) {
+      this.numberPhoto = 1;
+    }
+    if(this.numberPhoto > this.range.max) {
+      this.numberPhoto = 100;
+    }
   }
 
 }
--- src/app/modules/settings/settings.module.ts
+++ src/app/modules/settings/settings.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings.component';
 import { SharedModule } from '../shared/shared.module';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [SettingsComponent],
   imports: [
     CommonModule,
     SettingsRoutingModule,
-    SharedModule
+    FormsModule,
+    SharedModule,
+    MatInputModule,
+    MatGridListModule,
+    MatSelectModule,
   ]
 })
 export class SettingsModule { }
